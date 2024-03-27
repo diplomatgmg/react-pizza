2daf8351e5c4eb1e243db558ff1aef3243a6f107
Кирилл Попов <diplomatgmg@gmail.com>
2024-03-27 14:40:59 +0300

    Обновлена конфигурация развертывания backend

---
+++ backend/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+./wait-for-it.sh database:5432 -- echo "Database is ready"
+
+python manage.py migrate
+python manage.py loaddata __fixtures__/ingredient.json
+python manage.py loaddata __fixtures__/pizza.json
+python manage.py loaddata __fixtures__/user.json
+
+python manage.py runserver 0.0.0.0:8000
