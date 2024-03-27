3ec3768394526ed0449e41ea5e1b3bac5b266993
Кирилл Попов <diplomatgmg@gmail.com>
2024-03-27 22:22:13 +0300

    Убран нереализованный код

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,12 +1,10 @@
 import React, { type ReactElement } from 'react'
 import PizzaList from './Pizza/PizzaList'
-import Error from './Error'
 
 const Main = (): ReactElement => {
   return (
     <main className="main">
-      <p className="main__title">Все пиццы</p>
-      <Error />
+      {/* <Error /> */}
       <div className='pizza'>
         <PizzaList/>
       </div>
--- frontend/src/components/Sort/Sort.tsx
+++ frontend/src/components/Sort/Sort.tsx
@@ -1,6 +1,5 @@
 import React, { type ReactElement } from 'react'
 import ArrowTopSvg from '../../assets/svg/arrow-top.svg'
-import SortList from './SortList'
 
 const Sort = (): ReactElement => {
   return (
@@ -8,7 +7,7 @@ const Sort = (): ReactElement => {
      <img className="sort__icon" src={ArrowTopSvg} alt=""/>
      <p className="sort__text">Сортировка по:</p>
      <span className="sort__criteria">популярности</span>
-     <SortList />
+     {/* <SortList /> */}
    </div>
   )
 }
