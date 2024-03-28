788fc93c2de330bd72c9ff9eee4a4f1589493b20
Кирилл Попов <diplomatgmg@gmail.com>
2024-03-28 22:48:49 +0300

    Update startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,8 +1,10 @@
 ./wait-for-it.sh database:5432 -- echo "Database is ready"
 
 python manage.py migrate
-python manage.py loaddata __fixtures__/ingredient.json
-python manage.py loaddata __fixtures__/pizza.json
+
 python manage.py loaddata __fixtures__/user.json
+python manage.py loaddata __fixtures__/ingredient.json
+python manage.py loaddata __fixtures__/category.json
+python manage.py loaddata __fixtures__/pizza.json
 
 python manage.py runserver 0.0.0.0:8000
