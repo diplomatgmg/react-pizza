e32a6cd3026b9bb4e584ed608e216ad5d0c810c4
Кирилл Попов <diplomatgmg@gmail.com>
2024-04-26 12:04:24 +0300

    Добавлен эндпоинт

--- backend/apps/pizza/urls.py
+++ backend/apps/pizza/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PizzaListAPIView, CategoryListAPIView, PizzaDetailAPIView
+from .views import PizzaListAPIView, CategoryListAPIView, PizzaDetailAPIView, ExampleApiView
 
 urlpatterns = [
     path("pizzas/", PizzaListAPIView.as_view(), name="pizza-list"),
     path("pizzas/<int:pk>/", PizzaDetailAPIView.as_view(), name="pizza-detail"),
     path("categories/", CategoryListAPIView.as_view(), name="category-list"),
+    path('example/' , ExampleApiView.as_view())
 ]
